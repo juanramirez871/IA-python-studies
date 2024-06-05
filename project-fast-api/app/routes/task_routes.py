8789064764779a879cfbda3609daafd5a6510005
juan ramirez <juandiegoramirez071@gmail.com>
2024-06-05 07:27:57 -0500

    feat: :sparkles: crud of tasks

--- project-fast-api/app/routes/task_routes.py
+++ project-fast-api/app/routes/task_routes.py
@@ -9,11 +9,63 @@ router = APIRouter()
 async def read_task(item_id: int):
     conn = await connect_db()
     try:
-        query = "SELECT * FROM items WHERE id = $1"
+        query = "SELECT * FROM tasks WHERE id = $1"
         item = await conn.fetchrow(query, item_id)
         if item:
             return dict(item)
         else:
             raise HTTPException(status_code=404, detail="Item not found")
+    finally:
+        await disconnect_db(conn)
+        
+@router.get("/tasks")
+async def read_tasks():
+    conn = await connect_db()
+    try:
+        query = "SELECT * FROM tasks"
+        items = await conn.fetch(query)
+        return [dict(item) for item in items]
+    finally:
+        await disconnect_db(conn)
+        
+@router.post("/tasks")
+async def create_task(item: dict):
+    conn = await connect_db()
+    try:
+        query = "INSERT INTO tasks (title, description, team_id) VALUES ($1, $2, $3) RETURNING *"
+        item = await conn.fetchrow(query, item["name"], item["description"], item["team_id"])
+        return dict(item)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        await disconnect_db(conn)
+
+@router.put("/tasks/{item_id}")
+async def update_task(item_id: int, item: dict):
+    conn = await connect_db()
+    try:
+        query = "UPDATE tasks SET title = $1, description = $2, team_id = $3 WHERE id = $4 RETURNING *"
+        item = await conn.fetchrow(query, item["name"], item["description"], item["team_id"], item_id)
+        if item:
+            return dict(item)
+        else:
+            raise HTTPException(status_code=404, detail="Item not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        await disconnect_db(conn)
+        
+@router.delete("/tasks/{item_id}")
+async def delete_task(item_id: int):
+    conn = await connect_db()
+    try:
+        query = "DELETE FROM tasks WHERE id = $1 RETURNING *"
+        item = await conn.fetchrow(query, item_id)
+        if item:
+            return dict(item)
+        else:
+            raise HTTPException(status_code=404, detail="Item not found")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
     finally:
         await disconnect_db(conn)
